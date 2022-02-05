5f2d2433a0b94430677c9aa47506b5bbbc9e2b4d
maru_koch <nwokochafranklyn@gmail.com>
2022-02-05 16:07:50 +0100

    Wrestling with responsiveness

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,4 +3,14 @@ function socialMedia(id, image){
     whatsappImg.src = "../icons/"+`${image}`;
 }
 
-
+// function initialize() {
+//     alert("page loaded")
+//     var mapCanvas = document.getElementById('map');
+//     var mapOptions = {
+//       center: new google.maps.LatLng(44.5403, -78.5463),
+//       zoom: 8,
+//       mapTypeId: google.maps.MapTypeId.ROADMAP
+//     }
+//     var map = new google.maps.Map(mapCanvas, mapOptions)
+//   }
+//   google.maps.event.addDomListener(window, 'load', initialize);
